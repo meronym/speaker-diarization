78d9c4907de8fd78aeec117604d8e43acd2b2502
Lucian Boca <lucianboca@gmail.com>
2023-03-27 00:28:28 +0100

    chore: model weights details

--- predict.py
+++ predict.py
@@ -1,3 +1,7 @@
+"""
+download model weights to /data
+wget wget -O - https://pyannote-speaker-diarization.s3.eu-west-2.amazonaws.com/data-2023-03-25-02.tar.gz | tar xz -C /
+"""
 import json
 import tempfile
 
